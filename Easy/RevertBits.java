ba48163fd581ced6e6173ebb044f764f4a54c555
WyhLjn <wyhljn@gmail.com>
2019-07-17 18:54:58 +0800

    190,191

---
+++ Easy/RevertBits.java
@@ -0,0 +1,54 @@
+package com.wyh.me.leetcode.easy;
+
+/**
+ * Title: RevertBits.java<br>
+ * Description:
+ *
+ * Reverse bits of a given 32 bits unsigned integer.
+ *
+ * Example 1:
+ *
+ * Input: 00000010100101000001111010011100
+ * Output: 00111001011110000010100101000000
+ * Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+ *
+ * Example 2:
+ *
+ * Input: 11111111111111111111111111111101
+ * Output: 10111111111111111111111111111111
+ * Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10101111110010110010011101101001.
+ *
+ *
+ * Note:
+ *
+ * Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
+ * In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ *
+ * <br>
+ * Copyright: Copyright (c) 2019<br>
+ *
+ * @author wyh
+ * @date 2019年05月21日 19:09:00
+ */
+public class RevertBits {
+
+    public int reverseBits(int n) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++, n >>= 1) {
+            // n >>= 1，每次右移一位
+            // n & 1，用来取最后一位
+            // ret左移之后肯定是0，或运算保留原值
+            ret = ret << 1 | (n & 1);
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int ret = new RevertBits().reverseBits(n);
+        System.out.println("origin bits:" + Integer.toBinaryString(n));
+        System.out.println("target bits:" + Integer.toBinaryString(ret));
+        System.out.println("target bits:" + Integer.toBinaryString(0 << 10));
+    }
+}
